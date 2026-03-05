ebb8b4136f472557d077cf64000e9f7d7e0acd56
Pablo <pabloderivas3@gmail.com>
2026-03-05 15:29:00 -0300

    Added all keys, next finish keyboard overlay appearence

--- src/key.jsx
+++ src/key.jsx
@@ -1,7 +1,9 @@
-export default function Key({name='a', width='10', pressed=false}){
+export default function Key({name='a', width='10', pressed=false, className=false}){
     return(
-        <div className={`inline-block min-w-12 px-3 py-2 border-2 rounded-sm text-center 
-        ${pressed ? '':'border-[#7209b7]'}`}>
+        <div className={`
+            ${className ? className:'inline-block min-w-15 min-h-15'}
+            px-3 py-2 border-2 rounded-sm text-center
+            ${pressed ? 'font-bold text-lg':'border-[#7209b7]'}`}>
             <p>{name}</p>
         </div>
     )
--- src/keyboard.jsx
+++ src/keyboard.jsx
@@ -5,100 +5,156 @@ import Key from "./key"
 export default function Keyboard(){
     let [keys, setKeys] = useState(() => new Set())
     const row1 = [
-                [
-                    ['Esc', 'Escape'],
-                ],
-                [
-                    ['F1', 'F1'],
-                    ['F2', 'F2'],
-                    ['F3', 'F3'],
-                    ['F4', 'F4'],
-                ],
-                [
-                    ['F5', 'F5'],
-                    ['F6', 'F6'],
-                    ['F7', 'F7'],
-                    ['F8', 'F8'],
-                ],
-                [
-                    ['F9', 'F9'],
-                    ['F10', 'F10'],
-                    ['F11', 'F11'],
-                    ['F12', 'F12'],
-                ]
+                    [
+                        ['Esc', 'Escape'],
+                    ],
+                    [
+                        ['F1', 'F1'],
+                        ['F2', 'F2'],
+                        ['F3', 'F3'],
+                        ['F4', 'F4'],
+                    ],
+                    [
+                        ['F5', 'F5'],
+                        ['F6', 'F6'],
+                        ['F7', 'F7'],
+                        ['F8', 'F8'],
+                    ],
+                    [
+                        ['F9', 'F9'],
+                        ['F10', 'F10'],
+                        ['F11', 'F11'],
+                        ['F12', 'F12'],
+                    ]
                 ]
     const row2 = [
-                ['`', 'Backquote'],
-                ['1', 'Digit1'],
-                ['2', 'Digit2'],
-                ['3', 'Digit3'],
-                ['4', 'Digit4'],
-                ['5', 'Digit5'],
-                ['6', 'Digit6'],
-                ['7', 'Digit7'],
-                ['8', 'Digit8'],
-                ['9', 'Digit9'],
-                ['0', 'Digit0'],
-                ['-', 'Minus'],
-                ['=', 'Equal'],
-                ['Backspace', 'Backspace'],
+                    ['`', 'Backquote'],
+                    ['1', 'Digit1'],
+                    ['2', 'Digit2'],
+                    ['3', 'Digit3'],
+                    ['4', 'Digit4'],
+                    ['5', 'Digit5'],
+                    ['6', 'Digit6'],
+                    ['7', 'Digit7'],
+                    ['8', 'Digit8'],
+                    ['9', 'Digit9'],
+                    ['0', 'Digit0'],
+                    ['-', 'Minus'],
+                    ['=', 'Equal'],
+                    ['Backspace', 'Backspace'],
                 ]
     const row3 = [
-                ['Tab', 'Tab'],
-                ['q', 'KeyQ'],
-                ['w', 'KeyW'],
-                ['e', 'KeyE'],
-                ['r', 'KeyR'],
-                ['t', 'KeyT'],
-                ['y', 'KeyY'],
-                ['u', 'KeyU'],
-                ['i', 'KeyI'],
-                ['o', 'KeyO'],
-                ['p', 'KeyP'],
-                ['[', 'BracketLeft'],
-                [']', 'BracketRight'],
-                ['\\', 'Backslash'],
+                    ['Tab', 'Tab', 'min-w-22 min-h-15'],
+                    ['q', 'KeyQ'],
+                    ['w', 'KeyW'],
+                    ['e', 'KeyE'],
+                    ['r', 'KeyR'],
+                    ['t', 'KeyT'],
+                    ['y', 'KeyY'],
+                    ['u', 'KeyU'],
+                    ['i', 'KeyI'],
+                    ['o', 'KeyO'],
+                    ['p', 'KeyP'],
+                    ['[', 'BracketLeft'],
+                    [']', 'BracketRight'],
+                    ['\\', 'Backslash'],
                 ]
     const row4 = [
-                ['CapsLock', 'CapsLock'],
-                ['a', 'KeyA'],
-                ['s', 'KeyS'],
-                ['d', 'KeyD'],
-                ['f', 'KeyF'],
-                ['g', 'KeyG'],
-                ['h', 'KeyH'],
-                ['j', 'KeyJ'],
-                ['k', 'KeyK'],
-                ['l', 'KeyL'],
-                [';', 'Semicolon'],
-                ["'", 'Quote'],
-                ['Enter', 'Enter'],
+                    ['CapsLock', 'CapsLock', 'min-w-27 min-h-15'],
+                    ['a', 'KeyA'],
+                    ['s', 'KeyS'],
+                    ['d', 'KeyD'],
+                    ['f', 'KeyF'],
+                    ['g', 'KeyG'],
+                    ['h', 'KeyH'],
+                    ['j', 'KeyJ'],
+                    ['k', 'KeyK'],
+                    ['l', 'KeyL'],
+                    [';', 'Semicolon'],
+                    ["'", 'Quote'],
+                    ['Enter', 'Enter'],
                 ]
     const row5 = [
-                ['Shift', 'ShiftLeft'],
-                ['z', 'KeyZ'],
-                ['x', 'KeyX'],
-                ['c', 'KeyC'],
-                ['v', 'KeyV'],
-                ['b', 'KeyB'],
-                ['n', 'KeyN'],
-                ['m', 'KeyM'],
-                [',', 'Comma'],
-                ['.', 'Period'],
-                ['/', 'Slash'],
-                ['Shift', 'ShiftRight'],
+                    ['Shift', 'ShiftLeft', 'min-w-32 min-h-15'],
+                    ['z', 'KeyZ'],
+                    ['x', 'KeyX'],
+                    ['c', 'KeyC'],
+                    ['v', 'KeyV'],
+                    ['b', 'KeyB'],
+                    ['n', 'KeyN'],
+                    ['m', 'KeyM'],
+                    [',', 'Comma'],
+                    ['.', 'Period'],
+                    ['/', 'Slash'],
+                    ['Shift', 'ShiftRight'],
                 ]
     const row6 = [
-                ['Control', 'ControlLeft'],
-                ['Meta', 'MetaLeft'],   // Windows / Cmd
-                ['Alt', 'AltLeft'],
-                ['Space', 'Space'],
-                ['Alt', 'AltRight'],
-                ['Meta', 'MetaRight'],
-                ['ContextMenu', 'ContextMenu'],
-                ['Control', 'ControlRight'],
+                    ['Control', 'ControlLeft'],
+                    ['Meta', 'MetaLeft'],   // Windows / Cmd
+                    ['Alt', 'AltLeft'],
+                    ['Space', 'Space'],
+                    ['Alt', 'AltRight'],
+                    ['Meta', 'MetaRight'],
+                    ['ContextMenu', 'ContextMenu'],
+                    ['Control', 'ControlRight'],
                 ]
-    
+    const specialNav =[
+                    ['PrtScr', 'PrintScreen'],
+                    ['Scroll Lock', 'ScrollLock'],
+                    ['Pause', 'Pause'],
+                ]
+    const navigation = [
+                    [
+                        ['Ins', 'Insert'],
+                        ['Home', 'Home'],
+                        ['Page Up', 'PageUp'],
+                    ],
+                    [
+                        ['Delete', 'Delete'],
+                        ['End', 'End'],
+                        ['Page Down', 'PageDown'],
+                    ]
+                ]
+    const arrows = [
+                    [
+                        ['Up', 'ArrowUp'],
+                    ],
+                    [
+                        ['Left', 'ArrowLeft'],
+                        ['Down', 'ArrowDown'],
+                        ['Right', 'ArrowRight'],
+                    ]
+                ]
+    const numPad = [
+                    [
+                        ['Num Lock', 'NumLock'],
+                        ['/', 'NumpadDivide'],
+                        ['*', 'NumpadMultiply'],
+                        ['-', 'NumpadSubtract']
+                    ],
+                    [
+                        ['7', 'Numpad7'],
+                        ['8', 'Numpad8'],
+                        ['9', 'Numpad9'],
+                        ['+', 'NumpadAdd'],
+                    ],
+                    [
+                        ['4', 'Numpad4'],
+                        ['5', 'Numpad5'],
+                        ['6', 'Numpad6'],
+                    ],
+                    [
+                        ['1', 'Numpad1'],
+                        ['2', 'Numpad2'],
+                        ['3', 'Numpad3'],
+                        ['Enter', 'NumpadEnter'],
+                    ],
+                    [
+                        ['0', 'Numpad0'],
+                        ['.', 'NumpadDecimal'],
+                    ]
+                ]
+                
     useEffect(()=>{
         const handleKeyDown = (e) => {
             if (e.key != 'F12'){
@@ -143,7 +199,7 @@ export default function Keyboard(){
     }, [])
 
     return(
-        <div>
+        <div className="flex flex-row">
             {/* {[...keys].map((keyPress, key) => {
                 return(<div>
                     {keyPress}
@@ -162,29 +218,65 @@ export default function Keyboard(){
                 </div>
                 <div className="flex gap-2">
                     {row2.map((keyMayor, k) =>{
-                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])}/>)
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
                     })}
                 </div>
                 <div className="flex gap-2">
                     {row3.map((keyMayor, k) =>{
-                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])}/>)
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
                     })}
                 </div>
                 <div className="flex gap-2">
                     {row4.map((keyMayor, k) =>{
-                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])}/>)
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
                     })}
                 </div>
                 <div className="flex gap-2">
                     {row5.map((keyMayor, k) =>{
-                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])}/>)
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
                     })}
                 </div>
                 <div className="flex gap-2">
                     {row6.map((keyMayor, k) =>{
-                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])}/>)
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
                     })}
                 </div>
+            </div>
+
+            <div className="">
+                <div>
+                    {specialNav.map((keyMayor, k) => {
+                        return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
+                    })}
+                </div>
+                <div>
+                    {navigation.map((group, k) => (
+                        <div>
+                            {group.map((keyMayor, k) => {
+                                return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} />)
+                            })}
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    {arrows.map((group, k) => (
+                        <div>
+                            {group.map((keyMayor, k) => {
+                                return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} />)
+                            })}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div>
+                {numPad.map((group, k) => (
+                    <div>
+                        {group.map((keyMayor, k) => {
+                            return(<Key name={keyMayor[0]} pressed={keys.has(keyMayor[1])} className={keyMayor[2]}/>)
+                        })}
+                    </div>
+                ))}
             </div>
         </div>
     )
